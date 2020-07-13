7f1ca5fd2269c89b374ac3f53230fcefe1dc8d49
Meto Trajkovski <metot@hotmail.com>
2020-07-13 22:26:13 +0200

    Updated from master

--- js/balls.js
+++ js/balls.js
@@ -7,6 +7,11 @@
         In the horizontal simulation, a ball loses energy when bouncing from a wall (the wall is harder and stationary) and air resistence.
         The ball also loses energy from the air resistence, hitting the ground, rolling on the ground and gravity in the vertical simulation
         (but not from spinning and some other 3d things possible in billiard and basketball).
+
+        Known issue:
+        In "vertical" space/direction when the bottom is full with balls (when there is no space for a new ball)
+        adding a new ball will make all balls go crazy (jumping randomly). This is because the balls will always
+        collide and won't lose energy from colliding (I'm not sure how to solve this).
     *********************************************************/
 
     /**************
@@ -25,42 +30,83 @@
         };
     }
 
+    function moveBallsOutOfCollision(ball1, ball2) {
+        /*********************************************************
+            Find the positions of the balls when the collision occurred.
+            (because right they have collided - they're overlapping)
+
+            old ball1.position = ball1.position - T * ball1.velocity
+            old ball2.position = ball2.position - T * ball2.velocity
+
+            In this moment T is unknown. Solve this equation to find T:
+            distance(old ball1.position, old ball2.position) = (ball1.radius + ball2.radius)
+
+            This can be solved using the Quadratic formula, because after simplifying
+            the left side of the equation we'll get something like: a*(T^2) + b*T + c = 0
+        *********************************************************/
+        var v = ball1.velocity.sub(ball2.velocity);
+        var p = ball1.position.sub(ball2.position);
+        var r = ball1.radius + ball2.radius;
+
+        // quadratic formula coeficients
+        var a = v.X*v.X + v.Y*v.Y;
+        var b = (-2)*(p.X*v.X + p.Y*v.Y);
+        var c = p.X*p.X + p.Y*p.Y - r*r;
+
+        // quadratic formula discriminant
+        var d = b*b - 4*a*c;
+
+        // t1 and t2 from the quadratic formula (need only the positive solution)
+        var t = (-b - Math.sqrt(d)) / (2*a);
+        if (t < 0)
+            t = (-b + Math.sqrt(d)) / (2*a);
+
+        // calculate the old positions (positions when the collision occurred)
+        var oldPosition1 = ball1.position.sub(ball1.velocity.mult(t));
+        var oldPosition2 = ball2.position.sub(ball2.velocity.mult(t));
+
+        var maxChange = ball1.radius * 3;
+
+        if ((a == 0) || (d < 0) ||
+            (oldPosition1.distance(ball1.position) > maxChange) ||
+            (oldPosition2.distance(ball2.position) > maxChange)) {
+            // 1) if 'a' is zero then both balls have equal velocities, no solution
+            // 2) the discriminant shouldn't be negative in this simulation, but just in case check it
+            // 3) the chages are too big, something is wrong
+
+            if (ball1.position.distance(ball2.position) == 0) {
+                // move only one ball up
+                ball1.position = ball1.position.add(new Vector2D(0, -r));
+            } else {
+                // move both balls using the vector between these positions
+                var diff = (r - ball1.position.distance(ball2.position)) / 2;
+                ball1.position = ball1.position.add(ball1.position.sub(ball2.position).tryNormalize().mult(diff));
+                ball2.position = ball2.position.add(ball2.position.sub(ball1.position).tryNormalize().mult(diff));
+            }
+        } else {
+            // use the old positions
+            ball1.position = oldPosition1;
+            ball2.position = oldPosition2;
+        }
+    }
+
     Ball.prototype.collision = function(ball) {
-        var minDistance = ball.radius + this.radius;
-        var positionSub = this.position.sub(ball.position);
-        var distance = positionSub.length();
+        if (this.position.distance(ball.position) <= ball.radius + this.radius) {
+            moveBallsOutOfCollision(this, ball);
 
-        if (distance <= minDistance) {
-            if (distance != 0) {
-                /*********************************************************
-                    The formula could be found here: https://en.wikipedia.org/wiki/Elastic_collision
-                    velocityA -= (dot(velocityAB_sub, positionAB_sub) / distance^2) * positionAB_sub
-                    velocityB -= (dot(velocityBA_sub, positionBA_sub) / distance^2) * positionBA_sub
-                    but this thing (dot(velocityAB_sub, positionAB_sub) / distance^2) is same for 2 velocities
-                    because dot and length methods are commutative properties, and velocityAB_sub = -velocityBA_sub, same for positionSub
-                *********************************************************/
-                var coeff = this.velocity.sub(ball.velocity).dot(positionSub) / (distance * distance);
-                this.velocity = this.velocity.sub(positionSub.mult(coeff));
-                ball.velocity = ball.velocity.sub(positionSub.opposite().mult(coeff));
-            }
+            var positionSub = this.position.sub(ball.position);
+            var distance = positionSub.length();
 
-            // apply a random vector if both velocities are zero vectors
-            var applyRandomVector = this.velocity.isZero() && ball.velocity.isZero();
-            if (applyRandomVector) {
-                this.velocity = Vector2D.random();
-                ball.velocity = this.velocity.opposite();
-            }
-
-            // move balls outside of collision
-            var diff = (minDistance - distance) / 2 + 0.001; // add a very small value so they won't touch
-            this.position = this.position.add(this.velocity.tryNormalize().mult(diff));
-            ball.position = ball.position.add(ball.velocity.tryNormalize().mult(diff));
-
-            // if a random vector is applied revert the zero vectors
-            if (applyRandomVector) {
-                this.velocity = Vector2D.zero();
-                ball.velocity = Vector2D.zero();
-            }
+            /*********************************************************
+                The formula could be found here: https://en.wikipedia.org/wiki/Elastic_collision
+                velocityA -= (dot(velocityAB_sub, positionAB_sub) / distance^2) * positionAB_sub
+                velocityB -= (dot(velocityBA_sub, positionBA_sub) / distance^2) * positionBA_sub
+                but this thing (dot(velocityAB_sub, positionAB_sub) / distance^2) is same for 2 velocities
+                because dot and length methods are commutative properties, and velocityAB_sub = -velocityBA_sub, same for positionSub
+            *********************************************************/
+            var coeff = this.velocity.sub(ball.velocity).dot(positionSub) / (distance * distance);
+            this.velocity = this.velocity.sub(positionSub.mult(coeff));
+            ball.velocity = ball.velocity.sub(positionSub.opposite().mult(coeff));
         }
     }
 
